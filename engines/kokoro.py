e8e3c93a992b063164fd49e84235a3f820b5d260
Maxi <max.fondse@gmail.com>
2025-11-03 16:03:19 +0100

    cleanup kokoro engine

--- engines/kokoro.py
+++ engines/kokoro.py
@@ -1,10 +1,8 @@
 import io
-import time
-import urllib.request
 import soundfile as sf
-import os
 
 import torch
+import json
 
 from engines.base import BaseEngine
 from models import Voice
@@ -14,48 +12,67 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 
+heart_fallback = {
+    "af_heart": {
+        "name": "Heart",
+        "gender": "Female",
+        "lang_code": {
+            "espeak": "en-us",
+            "misaki": "a"
+        }
+    }
+}
+
 class KokoroEngine(BaseEngine):
     def __init__(self):
-        self._g2p = None
-        self.loaded_voice = 'af_heart'
+        self.loaded_voice = ""
+        self.voice_dict = None
+        voice_dict_path = Path(Path.cwd() / "resources/kokoro_voices.json")
+
+        if voice_dict_path.exists():
+            with open(voice_dict_path, "r") as f:
+                self.voice_dict = json.load(f)
+        else:
+            self.voice_dict = heart_fallback
 
         logging.info("Loading Kokoro model...")
-        self._model = KModel('hexgrad/Kokoro-82M').to('cuda' if os.environ.get('IZABELA_USE_CUDA') else 'cpu').eval()
-        self._pipeline = KPipeline('a', model=self._model)
+        self._model = KModel(repo_id='hexgrad/Kokoro-82M').eval()
+        self._pipeline = None
+
+        # TODO: Add cpu and gpu resolver
 
         # TODO: figure out how to get triton working on Windows so we can compile models
         # logging.info("Compiling Kokoro model. This might take some time, but will make the model run much faster.")
         # self._model.compile()
 
     def list_voices(self) -> list[Voice]:
-        lang_map = {'a': 'en-us', 'b': 'en-gb', 'e': 'es', 'f': 'fr-fr', 'h': 'hi', 'i': 'it', 'p': 'pt-br', 'j': 'ja', 'z': 'zh'}
-        voices = []
-        for voice in ["af_alloy", "af_aoede", "af_bella", "af_heart", "af_jessica", "af_kore", "af_nicole", "af_nova", "af_river", "af_sarah", "af_sky", "am_adam", "am_echo", "am_eric", "am_fenrir", "am_liam", "am_michael", "am_onyx", "am_puck", "am_santa", "bf_alice", "bf_emma", "bf_isabella", "bf_lily", "bm_daniel", "bm_fable", "bm_george", "bm_lewis", "ef_dora", "em_alex", "em_santa", "ff_siwis", "hf_alpha", "hf_beta", "hm_omega", "hm_psi", "if_sara", "im_nicola", "jf_alpha", "jf_gongitsune", "jf_nezumi", "jf_tebukuro", "jm_kumo", "pf_dora", "pm_alex", "pm_santa", "zf_xiaobei", "zf_xiaoni", "zf_xiaoxiao", "zf_xiaoyi", "zm_yunjian", "zm_yunxi", "zm_yunxia", "zm_yunyang"]:
-            voices.append(Voice(
-                id=voice,
-                name='Kokoro - ' + voice.split('_')[1].capitalize() + ' (' + ('Male' if voice[1] == 'm' else 'Female') + ') (' + lang_map[voice[0]] + ')',
+        voice_list = []
+        
+        for id in self.voice_dict:
+            voice_list.append(Voice(
+                id=id,
+                name=f"{self.voice_dict[id]["name"]} {self.voice_dict[id]["gender"]} - {self.voice_dict[id]["lang_code"]["espeak"]}",
                 category=self.__class__.__name__,
-                languageCode=lang_map[voice[0]]
+                languageCode=self.voice_dict[id]["lang_code"]["espeak"]
             ))
 
-        return voices
+        return voice_list
 
     def synthesize_voice(self, voice: Voice, text: str) -> bytes:
-        if self.loaded_voice[0] != voice.id[0]:
+        if self.loaded_voice != voice.id:
             logging.info(f'Loading pipeline for language {voice.languageCode}...')
-            self._pipeline = KPipeline(voice.id[0], model=self._model)
+            self._pipeline = KPipeline(lang_code=self.voice_dict[voice.id]["lang_code"]["misaki"], model=self._model)
             self.loaded_voice = voice.id
 
-        start_time = time.time()
-        generator = self._pipeline(text, voice=voice.id)
         samples = None
-        for i, (gs, ps, audio) in enumerate(generator):
-            if samples is None:
-                samples = audio
-            else:
-                samples = torch.cat((samples, audio))
-        end_time = time.time()
-        logging.info(f"Generation took {end_time - start_time} seconds")
+
+        for result in self._pipeline(text, voice=voice.id):
+            if result.audio is not None:
+                if samples is None:
+                    samples = result.audio
+                else:
+                    samples = torch.cat((samples, result.audio))
+        
         mp3_bytes = io.BytesIO()
         sf.write(file=mp3_bytes, samplerate=24000, data=samples, format='MP3', bitrate_mode='VARIABLE', compression_level=0.25)
-        return mp3_bytes.getvalue()
+        return mp3_bytes.getvalue() 
